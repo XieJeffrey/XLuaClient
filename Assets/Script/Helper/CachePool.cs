fd264e6612cfac737d50e3befed172d5807e73db
XieJeffrey <32788665+XieJeffrey@users.noreply.github.com>
2017-11-10 15:12:34 +0800

    修改计时器

---
+++ Assets/Script/Helper/CachePool.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CachePool  : MonoBehaviour  {
+
+    private Dictionary<string, List<Object>> mPool = new Dictionary<string, List<Object>>();
+
+    //private Object Get(string prefabName, bool isActive = false)
+    //{
+    //    if(mPool.ContainsKey(prefabName))
+            
+    //}
+
+}
